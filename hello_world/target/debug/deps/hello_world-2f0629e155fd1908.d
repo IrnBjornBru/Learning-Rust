0a165a11cddce91149a5930cfa7d0c1ec1e04372
IrnBjornBru <clarkenat71@gmail.com>
2022-10-09 17:54:25 +0100

    Hello World!
    followed the tutorial for rust with vs code: https://code.visualstudio.com/docs/languages/rust?adlt=strict&toWww=1&redig=EC232EC9294447D6A3E5D299DC1DD55B

---
+++ hello_world/target/debug/deps/hello_world-2f0629e155fd1908.d
@@ -0,0 +1,5 @@
+c:\Users\clark_usj34pn\OneDrive\Documents\GitHub\Learning-Rust\hello_world\target\debug\deps\hello_world-2f0629e155fd1908.rmeta: src\main.rs
+
+c:\Users\clark_usj34pn\OneDrive\Documents\GitHub\Learning-Rust\hello_world\target\debug\deps\hello_world-2f0629e155fd1908.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/deps/hello_world-c88b5ce31b8b46e0.d
@@ -0,0 +1,5 @@
+c:\Users\clark_usj34pn\OneDrive\Documents\GitHub\Learning-Rust\hello_world\target\debug\deps\hello_world-c88b5ce31b8b46e0.rmeta: src\main.rs
+
+c:\Users\clark_usj34pn\OneDrive\Documents\GitHub\Learning-Rust\hello_world\target\debug\deps\hello_world-c88b5ce31b8b46e0.d: src\main.rs
+
+src\main.rs:
